c8a80e2278cd11022f2f2e2deab2c76c5d69c4bc
luowei <745054025@qq.com>
2015-06-17 16:40:03 +0800

    完善扫码功能...

--- MyAwareness/Setting/ScanQRViewController.m
+++ MyAwareness/Setting/ScanQRViewController.m
@@ -5,9 +5,10 @@
 
 #import "ScanQRViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ScanQRViewController () <AVCaptureMetadataOutputObjectsDelegate> {
-
+@interface ScanQRViewController () <AVCaptureMetadataOutputObjectsDelegate,UIAlertViewDelegate> {
+    NSString *stringValue;
 }
 
 @property(nonatomic, strong) AVCaptureDevice *device;
@@ -28,15 +29,24 @@
     self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     self.session = [AVCaptureSession new];
 
+
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"重新扫码" style:UIBarButtonItemStyleDone target:self action:@selector(reScan)];
+
     //todo:添加工具栏,图片及闪光开关
 
+}
+
+- (void)reScan {
+    if(!_session.isRunning){
+        [_session startRunning];
+    }
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
 
     [self setupCameral];
-    [self.session startRunning];
+//    [self.session startRunning];
 }
 
 - (void)setupCameral {
@@ -72,7 +82,7 @@
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects
        fromConnection:(AVCaptureConnection *)connection {
-    NSString *stringValue;
+
     if (metadataObjects.count > 0) {
         AVMetadataMachineReadableCodeObject *metadataObject = metadataObjects[0] == nil ? nil : (AVMetadataMachineReadableCodeObject *) metadataObjects[0];
         stringValue = metadataObject == nil ? nil : metadataObject.stringValue;
@@ -80,8 +90,29 @@
     [_session stopRunning];
     NSLog(@"code is %@",stringValue);
 
-    [[[UIAlertView alloc] initWithTitle:@"二维码" message:[NSString stringWithFormat:@"扫到的二维码内容为:%@", stringValue]
-                               delegate:nil cancelButtonTitle:@"确认" otherButtonTitles:nil] show];
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    [pasteboard setValue:stringValue forPasteboardType:(__bridge NSString *) kUTTypeUTF8PlainText];
+//    [pasteboard setValue:stringValue forPasteboardType:@"public.plain-text"];
+//    [pasteboard setValue:stringValue forPasteboardType:@"public.text"];
+
+    NSString *urlRegex = @"((http|ftp|https)://)(([a-zA-Z0-9\\._-]+\\.[a-zA-Z]{2,6})|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,4})*(/[a-zA-Z0-9\\&%_\\./-~-]*)?";
+    NSPredicate *urlStrPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",urlRegex];
+
+    //如果是Url
+    if([urlStrPredicate evaluateWithObject:stringValue]){
+        [[[UIAlertView alloc] initWithTitle:@"二维码" message:[NSString stringWithFormat:@"Url:%@ 已保存到剪粘板,是否在Safari中打开?", stringValue]
+                                   delegate:self cancelButtonTitle:@"不了" otherButtonTitles:@"打开",nil] show];
+    }else{
+        [[[UIAlertView alloc] initWithTitle:@"二维码" message:[NSString stringWithFormat:@"内容:%@ 已保存到剪粘板", stringValue]
+                                   delegate:nil cancelButtonTitle:@"确认" otherButtonTitles:nil] show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if(buttonIndex==1){
+        NSURL *url = [NSURL URLWithString:[stringValue stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        [[UIApplication sharedApplication] openURL:url];
+    }
 }
 
 @end
